dd3f654fd24cbe0f668a71f27fc6264f58acb998
Adedayo Sanni <asanni@student.42sp.org.br>
2023-07-28 22:30:44 -0300

    add a func memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,8 +6,35 @@
 /*   By: asanni <asanni@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/27 18:01:40 by asanni            #+#    #+#             */
-/*   Updated: 2023/07/27 18:01:41 by asanni           ###   ########.fr       */
+/*   Updated: 2023/07/28 22:29:44 by asanni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	char	*p_src;
+	char	*p_dest;
+
+	if (dest == src || !n)
+		return (dest);
+	p_src = (char *)src;
+	p_dest = (char *)dest;
+	while (n--)
+		*p_dest++ = *p_src++;
+	return (dest);
+}
+
+// #include <stdio.h>
+
+// int	main(void)
+// {
+// 	char	dest[] = "Adedayo";
+// 	char	src[] = "Linda";
+
+// 	printf("Este é o destino: %s\n", dest);
+// 	printf("Esta é a origem: %s\n", src);
+// 	ft_memcpy(dest, src, 10);
+// 	printf("Está é a função modificada: %s", dest);
+// }
